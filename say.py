767c44532719cd7ae62426976762b9c76a9342fd
Takumi <83392851+Takumi-senpai@users.noreply.github.com>
2021-04-29 09:41:22 +0200

    Add files via upload
    This file can help you if you want the "say" command.

---
+++ say.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands, tasks
+
+bot = commands.Bot(command_prefix = ";", description = "I don't know")
+
+@bot.command()
+@commands.has_permissions(manage_messages = True) #<=== To run the command you must have the "manage_messages"
+async def say(ctx, *texte):                       #permissions if you want to change this permissions, you can get the different permissions on https://discordpy.readthedocs.io/en/latest/api.html?highlight=permissions#discord.Permissions
+    await ctx.message.delete() #<=== You can delete this line if you want to keep the message your order after executing it
+    await ctx.send(" ".join(texte))
+
+bot.run("ODM1NDc3NjkzMDYzMjMzNTU5.YIQBPA.xQ0ZROD-lSWmNbLqFUSoUXAqZ2Y") #<=== Put the token between the quotes.
